0a0cd0789c7b97ae98b67a13b0f5a852286c26d2
Chris Sterk <chris.sterkenburg@usesoftware.com.au>
2024-03-24 18:54:36 +1000

    update default route for debugging, implement initial play route functionality

--- build/index.js
+++ build/index.js
@@ -3,15 +3,71 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/index.js
 const express_1 = __importDefault(require("express"));
+const child_process_1 = require("child_process");
+const os_1 = require("os");
+const body_parser_1 = __importDefault(require("body-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+/*
+ * Constants
+ */
+const PORT = process.env.PORT;
+const WINDOWS_PLATFORM = 'win32';
+const MAC_PLATFORM = 'darwin';
+/*
+ * Server setup
+ */
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
+const osPlatform = (0, os_1.platform)();
+/**
+ * Base Route - Used for debugging and ensuring the server is running.
+ *
+ * @returns The current port and platforn from which the server is running on
+ */
 app.get("/", (req, res) => {
-    res.send(`The server is running on port ${port}`);
+    res.send(`The server is running on port ${PORT}, on platform ${osPlatform}`);
 });
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+/**
+ * Play Route - Opens the provided url in the local
+ */
+app.post('/play', (req, res) => {
+    const { url } = req.body;
+    let browserCommand = `chromium-browser ${url}`;
+    // Check if Chromium is installed
+    (0, child_process_1.exec)('which chromium-browser', (error, stdout) => {
+        // If Chromium is not installed, use alternative browser instead per platform (used testing locally)
+        if (error || !stdout) {
+            if (osPlatform === WINDOWS_PLATFORM) {
+                browserCommand = "start microsoft-edge";
+            }
+            else if (osPlatform === MAC_PLATFORM) {
+                browserCommand = "open -a 'Google Chrome'";
+            }
+            else {
+                browserCommand = "google-chrome --no-sandbox";
+            }
+        }
+        // Open browser with the appropriate command
+        console.log(`executing command: ${browserCommand}`);
+        (0, child_process_1.exec)(`${browserCommand} ${url}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error opening browser: ${error.message}`);
+                return res.status(500).json({ error: 'Internal server error' });
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+            }
+            console.log(`Opened browser with URL: ${url}`);
+            res.json({ status: 'success' });
+        });
+    });
+});
+/**
+ * Start the server
+ */
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}, on platform ${osPlatform}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,80 @@
-// src/index.js
 import express, { Express, Request, Response } from "express";
+import { ExecException, exec } from "child_process";
+import { platform } from "os";
+import BodyParser from "body-parser";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+/*
+ * Constants 
+ */
+const PORT = process.env.PORT
+const WINDOWS_PLATFORM = 'win32';
+const MAC_PLATFORM = 'darwin';
 
+
+/*
+ * Server setup
+ */
+const app: Express = express();
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use(BodyParser.json())
+const osPlatform = platform();
+
+
+/**
+ * Base Route - Used for debugging and ensuring the server is running.
+ * 
+ * @returns The current port and platforn from which the server is running on
+ */
 app.get("/", (req: Request, res: Response) => {
-  res.send(`The server is running on port ${port}`);
+  res.send(`The server is running on port ${PORT}, on platform ${osPlatform}`);
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+
+/**
+ * Play Route - Opens the provided url in the local
+ */
+app.post('/play', (req: Request, res: Response) => {
+  const { url } = req.body;
+
+  // Default: Chromium Browser
+  let browserCommand = `chromium-browser`;
+
+  // Check if Chromium is installed
+  exec('which chromium-browser', (error: ExecException | null, stdout: string) => {
+
+    // If Chromium is not installed, use alternative browser instead per platform (used testing locally)
+    if (error || !stdout) {
+      if (osPlatform === WINDOWS_PLATFORM) {
+        browserCommand = "start microsoft-edge";
+      } else if (osPlatform === MAC_PLATFORM) {
+        browserCommand = "open -a 'Google Chrome'";
+      } else {
+        browserCommand = "google-chrome --no-sandbox";
+      }
+    }
+
+    // Open browser with the appropriate command
+    console.log(`executing command: ${browserCommand}`);
+    exec(`${browserCommand} ${url}`, (error: ExecException | null, stdout: string, stderr: string) => {
+      if (error) {
+        console.error(`Error opening browser: ${error.message}`);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+      }
+      console.log(`Opened browser with URL: ${url}`);
+      res.json({ status: 'success' });
+    });
+  });
+});
+
+
+/**
+ * Start the server
+ */
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}, on platform ${osPlatform}`);
 });
