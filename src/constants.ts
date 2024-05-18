e39fb382ac51bbb89c57f73ddf2148f4a9fd2a6c
Chris Sterk <chris.sterkenburg@usesoftware.com.au>
2024-05-18 18:28:16 +1000

    update WebSocketEventKeys to include new volcume keys

--- src/constants.ts
+++ src/constants.ts
@@ -7,18 +7,22 @@ export const YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3";
  * Ensure that the client and server **have matching event keys.**
  */
 export const WebSocketEventKeys = {
+  /** Used to return the current state to the newly connect client. */
+  getInitialState: "get-initial-state",
+  /** Gets the current video. */
+  currentVideo: "current-video",
   /** Any error values. */
   error: "error",
   /** If the player is loading. */
   isLoading: "is-loading",
   /** Gets the value from the isPlaying boolean. */
   isPlaying: "is-playing",
-  /** Updates the isPlaying boolean state. */
-  setIsPlaying: "set-is-playing",
-  /** Gets the current video. */
-  currentVideo: "current-video",
   /** Updates the current video and sets `isPlaying` boolean to true on the server. */
   setCurrentVideo: "set-current-video",
-  /** Used to return the current state to the newly connect client. */
-  getInitialState: "get-initial-state"
+  /** Updates the isPlaying boolean state. */
+  setIsPlaying: "set-is-playing",
+  /** Updates the volume of the operating system. */
+  setSystemVolume: "set-system-vol",
+  /** Gets the current ooperating system volume level. */
+  systemVolume: "system-vol"
 }
