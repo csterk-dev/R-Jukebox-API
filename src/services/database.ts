2a237ca756bea54d7a0919953b0fcb33d55fffff
Chris Sterk <chris.sterkenburg@usesoftware.com.au>
2025-08-02 20:02:48 +1000

    cont sim

--- src/services/database.ts
+++ src/services/database.ts
@@ -34,7 +34,6 @@ export async function initialiseDBConnection(): Promise<Database> {
 
 
 type InitialisedStateVarsReturn = {
-  // history: HistoryVideo[];
   queue: Video[];
   logs: EntryLog[];
 }
